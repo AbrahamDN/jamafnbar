15668db596545254ad4347ef413e67b25048821b
ORIGINALLYBLACK <ukoriginallyblack@gmail.com>
2026-08-30 23:05:45 +0100

    Integrate BlackCard widget script in layout
    Added a script tag for the BlackCard widget and adjusted the layout structure.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "./context.js/appContext";
 import Content from "./components/content";
+import Script from "next/script";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +16,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+  <body className={`${inter.className} overflow-x-hidden`}>
+
       <AppProvider>
-        <body className={`${inter.className} overflow-x-hidden`}>
           <Content>{children}</Content>
+        </AppProvider>
+  <script src="http://localhost:3000/api/blackcard/widget/v1.js" data-blackcard-key="bcw_live_JbrdBdBgAu9z8fBtPA7ZH5WB7ZAa6Dq4jdLfu4PTAjg" data-label="Open BlackCard" data-position="right" defer></script>
         </body>
-      </AppProvider>
     </html>
   );
 }
